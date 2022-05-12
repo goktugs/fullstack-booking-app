97472d17376ac6ac82a1eaab3b0cadfe95634e22
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-05-12 23:33:24 +0300

    Add routes

---
+++ server/routes/auth.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('auth endpoint');
+});
+
+router.get('/register', (req, res) => {
+  res.send('auth register endpoint');
+});
+
+export default router;
